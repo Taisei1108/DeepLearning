a0d39b381a1c1ab71345706b625bd79e7d78b61a
Taisei1108 <taikitigw7@gmail.com>
2021-09-27 19:01:37 +0900

    データセット関連の修正を行った(listでデータセット管理、クラスラベル作成など)

---
+++ CASIA_Segmentation/data_manage/make_datasetlist.py
@@ -0,0 +1,71 @@
+"""
+データセットをすべて読み込んで、指定した数で分割 -> パス名をこのディレクトリにtxtデータで保存するプログラム
+・train_list.txt
+・test_list.txt
+・val_list.txt
+が作られる予定
+
+実験でデータセットを使う場合はリストから読み込むようにする
+データセットROOT/サブディレクトリ(ラベルごとに0,1,...Nのように並んでいてほしい)
+"""
+#python make_datasetlist.py --dataset_root ../../../datasets/Columbia/data/ --class_num 2
+import argparse
+import glob
+import os
+import random
+
+random.seed(0)
+
+if __name__ == '__main__':
+
+    #引数の読み込み、データセットのルートとデータセットのサブディレクトリを指定する
+
+    parser = argparse.ArgumentParser()
+
+    parser.add_argument('--dataset_root',required=True,type=str)
+    parser.add_argument('--class_num',required=True,type=int)
+    parser.add_argument('--divide_ratio',default=(8,1,1),help='データセットの割合を決める、いい案が浮かばないのでタプルにする')
+
+    args = parser.parse_args()
+    print(vars(args))
+
+    all_files =[]
+
+    train_ratio = args.divide_ratio[0]/10
+    val_ratio = args.divide_ratio[1]/10
+    test_ratio =  args.divide_ratio[2]/10
+
+    #データをすべて取得する
+    for i in range(args.class_num):
+        files = glob.glob(args.dataset_root+str(i)+"/*")
+        all_files.append(files)
+
+    dataset = dict(train=[],val=[],test=[]) #0から train,val,testの順で打ち込む予定
+    
+    dataset_name =["train","val","test"]
+
+    for i in range(args.class_num):
+        shuffled_list = random.sample(all_files[i],len(all_files[i]))
+        dataset["train"] += shuffled_list[:int(len(shuffled_list)*train_ratio)] 
+        dataset["val"] += shuffled_list[int(len(shuffled_list)*train_ratio):int(-1*len(shuffled_list)*test_ratio)]
+        dataset["test"]  += shuffled_list[int(-1*len(shuffled_list)*test_ratio):]
+        #print(shuffled_list)
+        #print(shuffled_list[:int(len(shuffled_list)*train_ratio)] )
+    print(len(dataset["train"]))
+    print(len(dataset["val"]))
+    print(len(dataset["test"]))
+    random.shuffle(dataset["train"])
+    random.shuffle(dataset["val"])
+    random.shuffle(dataset["test"])
+
+    with open('train.txt', 'w') as f:
+        for d in dataset["train"]:
+            f.write("%s\n" % d.split('/')[-1].split('.')[0]) 
+
+    with open('val.txt', 'w') as f:
+        for d in dataset["val"]:
+            f.write("%s\n" % d.split('/')[-1].split('.')[0])
+
+    with open('test.txt', 'w') as f:
+        for d in dataset["test"]:
+            f.write("%s\n" % d.split('/')[-1].split('.')[0])
---
+++ DataLoader/Columbia2binary.py
@@ -0,0 +1,46 @@
+"""
+コロンビアのマスクを事前にバイナリ化するためのファイル
+image2segmentationとほぼ同じだが、なるべく汎用的にした
+
+"""
+
+import argparse
+import glob
+import os
+from PIL import Image
+
+from torchvision import transforms
+
+CROP_SIZE = 256
+transform=transforms.Compose([
+        transforms.Resize((CROP_SIZE,CROP_SIZE))
+])
+
+#python Columbia2binary.py  --Columbia_root ../../datasets/Columbia/data/
+
+if __name__ == '__main__':
+
+    
+    parser = argparse.ArgumentParser()
+
+    parser.add_argument('--Columbia_root',required=True,type=str)
+    parser.add_argument('--mask_dir',default="edgemask/",type=str)
+    parser.add_argument('--out',default="mask_binary/",type=str)
+    args = parser.parse_args()
+    print(vars(args))
+
+    os.makedirs(args.Columbia_root+args.out, exist_ok=True)
+
+    files = glob.glob(args.Columbia_root+args.mask_dir+"/*")
+    print(files)
+
+
+    for path in files:
+        if path.split(".")[-2][-10:]=="edgemask_3": #想定の入力'../../datasets/Columbia/data/edgemask/canong3_nikond70_sub_11_edgemask.jpg'
+            mask_image_gray = Image.open(path).convert('L')
+            mask_image_crop = transform(mask_image_gray)
+            mask_image_binary=mask_image_crop.point(lambda x: 0 if x < 90 else 255)#マスクの閾値はこれかな ~70,130~だと漏れるので
+            
+            path_name = path.split('/')[-1]
+
+            mask_image_binary.save(args.Columbia_root+args.out+path_name, quality=100)
